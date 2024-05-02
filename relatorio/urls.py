87fbd4537c9adce3fb717f10c89fcfb19a160093
Breno Nascimento <brenonsmacedo@gmail.com>
2024-05-02 11:54:20 -0400

    mudança nas rotas
    removi o a visualização da tabela de index, e dediquei um rota exclusiva para ela, para desenvolver de fato a pagina index desse programa.

--- relatorio/urls.py
+++ relatorio/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.selecao_tabela),
+    path('seleciona/', views.selecao_tabela),
     path('visualizacao/', views.visualizacao, name='visualizacao'),
     
 ]
