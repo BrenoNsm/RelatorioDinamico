7dd105fa048cc3ff552ed2881b20d64e73af136c
Breno Nascimento <brenonsmacedo@gmail.com>
2024-06-10 09:51:14 -0400

    Ajuste da tabela

--- relatorio/views.py
+++ relatorio/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.db import connection
+import pandas as pd
+
 
 def selecao_tabela(request):
     with connection.cursor() as cursor:
@@ -10,12 +12,32 @@ def selecao_tabela(request):
 
 def visualizacao(request):
     if request.method == 'POST':
-        selected_table = request.POST.get('selected_table')  # Corrigido para 'selected_table'
+        selected_table = request.POST.get('selected_table')
+        file_uploaded = request.FILES.get('file_upload')
+        
+
+        if file_uploaded:
+            if file_uploaded.name.endswith('.csv'):
+                df = pd.read_csv(file_uploaded)
+            elif file_uploaded.name.endswith('.xls') or file_uploaded.name.endswith('.xlsx'):
+                df = pd.read_excel(file_uploaded)
+            else:
+                return HttpResponse("Arquivo não suportado.")
+
+            # Verificar se o DataFrame não está vazio antes de renderizar
+            if not df.empty:
+                return render(request, 'relatorio/visualizacao.html', {'selected_table': selected_table, 'dataframe': df})
+            else:
+                return HttpResponse("O arquivo está vazio.")
+
         with connection.cursor() as cursor:
             cursor.execute(f"SELECT * FROM {selected_table}")
-            columns = [col[0] for col in cursor.description]  # Obter os nomes das colunas
+            columns = [col[0] for col in cursor.description]
             results = cursor.fetchall()
-        
-        return render(request, 'relatorio/visualizacao.html', {'columns': columns, 'results': results})
+
+        return render(request, 'relatorio/visualizacao.html', {'selected_table': selected_table, 'columns': columns, 'results': results})
     else:
         return selecao_tabela(request)
+    
+    
+
